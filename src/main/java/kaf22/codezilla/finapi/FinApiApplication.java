435d29069cb285a22988b5fa6b8b28b3c7373a39
Taras Bandura <56435471+Bandura1907@users.noreply.github.com>
2022-02-04 10:56:30 +0200

    add PersonController

--- src/main/java/kaf22/codezilla/finapi/FinApiApplication.java
+++ src/main/java/kaf22/codezilla/finapi/FinApiApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
 import java.util.Optional;
@@ -16,6 +18,18 @@ public class FinApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FinApiApplication.class, args);
+    }
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*");
+            }
+        };
     }
 
     @Bean
